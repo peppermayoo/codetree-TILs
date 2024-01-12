0fe4888d6aff1a63d3aeab8641096e11575f3224
peppermayoo <109782283+peppermayoo@users.noreply.github.com>
2024-01-13 04:47:11 +0900

    Title: 두 정수에 대한 연산값, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240113/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 240113/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -1,10 +1,16 @@
 def solution(x,y):
     max_num = max(x,y)
     min_num = min(x,y)
-    x = max_num+25
-    y = min_num*2
+    if x == max_num:
+      x = x + 25
+    elif x == min_num:
+      x = x * 2
+    if y == max_num:
+      y = y + 25
+    elif y == min_num:
+      y = y * 2
     return x,y
 
 a,b = map(int,input().split())
 changed_a, changed_b = solution(a,b)
-print(changed_b,changed_a)
+print(changed_a, changed_b)
