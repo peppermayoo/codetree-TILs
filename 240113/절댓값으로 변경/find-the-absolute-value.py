1bf23175198bf62e0dcc8d285cbd752fd65de221
peppermayoo <109782283+peppermayoo@users.noreply.github.com>
2024-01-13 06:35:54 +0900

    Title: 절댓값으로 변경, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240113/절댓값으로 변경/find-the-absolute-value.py
+++ 240113/절댓값으로 변경/find-the-absolute-value.py
@@ -1,11 +1,11 @@
 def solution(a):
     for i in range(len(a)):
-        a[i] = (abs(a[i]))
+        a[i] = abs(a[i])
     return a
 
-
 n = int(input())
-n_list = list(map(int,input().split()))
+n_list = list(map(int, input().split()))
+n_list = solution(n_list)
 
 for ele in n_list:
     print(ele, end=" ")
